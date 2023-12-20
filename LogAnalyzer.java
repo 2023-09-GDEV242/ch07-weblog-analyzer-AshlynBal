2785ed8849da78f680e24e67a12cf51c895db0f4
Ashlyn <AshlynBal04@gmail.com>
2023-12-19 19:46:10 -0500

    7.19

--- LogAnalyzer.java
+++ LogAnalyzer.java
@@ -1,12 +1,16 @@
 /**
- * Read web server data and analyse hourly access patterns.
+ * Read web server data and analyze hourly access patterns.
  *
  * @author Ashlyn Balicki, David J. Barnes, and Michael Kölling.
  * @version 2023.12.19
  */
 public class LogAnalyzer {
+    private int[] yearCounts;
+    private int[] monthCounts;
+    private int[] dayCounts;
     // Where to calculate the hourly access counts.
     private int[] hourCounts;
+    private int[] minuteCounts;
     // Use a LogfileReader to access the data.
     private LogfileReader reader;
 
@@ -14,9 +18,14 @@ public class LogAnalyzer {
      * Create an object to analyze hourly web accesses.
      */
     public LogAnalyzer() {
-        // Create the array object to hold the hourly
-        // access counts.
+        // Create the array object to hold the access counts.
+        // Year = 2000 + i
+        // Thank god this program will never be used after 2019
+        yearCounts = new int[20];
+        monthCounts = new int[12];
+        dayCounts = new int[31];
         hourCounts = new int[24];
+        minuteCounts = new int[60];
         // Create the reader to obtain the data.
         reader = new LogfileReader("demo.log");
     }
@@ -29,12 +38,19 @@ public class LogAnalyzer {
      * @param filename name of the LogfileReader.
      */
     public LogAnalyzer(String filename) {
-        // Create the array object to hold the hourly
-        // access counts.
+        // Create the array object to hold the access counts.
+        // Year = 2000 + i
+        // Thank god this program will never be used after 2019
+        yearCounts = new int[20];
+        monthCounts = new int[12];
+        dayCounts = new int[31];
         hourCounts = new int[24];
+        minuteCounts = new int[60];
         // Create the reader to obtain the data.
         reader = new LogfileReader(filename);
     }
+
+    //7.14
 
     /**
      * Return the number of accesses recorded in the log file.
@@ -58,10 +74,26 @@ public class LogAnalyzer {
         }
     }
 
+    //
+
     /**
-     * Print the hourly counts.
-     * These should have been set with a prior
-     * call to analyzeHourlyData.
+     * Analyze the year, month, day, hour, and minute data from the log file.
+     */
+    public void analyzeTimeData() {
+        while (reader.hasNext()) {
+            LogEntry entry = reader.next();
+            // Year = 2000 + i
+            yearCounts[entry.getYear() - 2000]++;
+            // January = month 0
+            monthCounts[entry.getMonth() - 1]++;
+            dayCounts[entry.getDay()]++;
+            hourCounts[entry.getHour()]++;
+            minuteCounts[entry.getMinute()]++;
+        }
+    }
+
+    /**
+     * Print the hourly counts. These should have been set with a prior call to analyzeHourlyData or analyzeTimeData.
      */
     public void printHourlyCounts() {
         System.out.println("Hr: Count");
@@ -70,14 +102,24 @@ public class LogAnalyzer {
         }
     }
 
+    /**
+     * Print the monthly counts. These should have been set with a prior call to analyzeTimeData.
+     */
+    public void totalAccessesPerMonth() {
+        System.out.println("Month: Count");
+        for (int i = 0; i < monthCounts.length; i++) {
+            System.out.println(i + ": " + monthCounts[i]);
+        }
+    }
+
     // 7.15
 
     /**
-     * Prints and returns the busiest hour in a year.
-     * If there are multiple hours tied for busiest,
-     * it says there's a tie and only prints the first.
+     * Returns the busiest hour.
+     * If there are multiple hours tied for busiest, it only returns the first.
+     * With no data, null is returned.
      *
-     * @return busiest hour in a year
+     * @return busiest hour
      */
     public int busiestHour() {
         int highestValue = 0;
@@ -93,26 +135,17 @@ public class LogAnalyzer {
                 tied = false;
             }
         }
-        if (highestValue == 0) {
-            System.out.println("No data");
-            return hour;
-        }
-        if (!tied) {
-            System.out.println("Hour " + hour + " is the busiest hour with " + highestValue + " uses.");
-        } else {
-            System.out.println("Hour " + hour + " is tied for the busiest hour with " + highestValue + " uses.");
-        }
         return hour;
     }
 
     // 7.16
 
     /**
-     * Returns the quietest consecutive hour in a year.
-     * If there are multiple hours tied for quietest,
-     * it says there's a tie and only gives the first.
+     * Returns the quietest hour.
+     * If there are multiple hours tied for quietest, it only gives the first.
+     * With no data, null is returned.
      *
-     * @return quietest hour in a year
+     * @return quietest hour
      */
     public int quietestHour() {
         int lowestValue = Integer.MAX_VALUE;
@@ -128,28 +161,118 @@ public class LogAnalyzer {
                 tied = false;
             }
         }
-        if (lowestValue == Integer.MAX_VALUE) {
-            System.out.println("No data");
-            return hour;
-        }
-        if (!tied) {
-            System.out.println("Hour " + hour + " is the quietest hour with " + lowestValue + " uses.");
-        } else {
-            System.out.println("Hour " + hour + " is tied for the quietest hour with " + lowestValue + " uses.");
-        }
         return hour;
     }
+
+    /**
+     * Returns the busiest day.
+     * If there are multiple days tied for busiest, it only returns the first.
+     * With no data, null is returned.
+     *
+     * @return busiest day
+     */
+    public int busiestDay() {
+        int highestValue = 0;
+        int day = -1;
+        boolean tied = false;
+        for (int i = 0; i < dayCounts.length; i++) {
+            if (dayCounts[i] == highestValue) {
+                tied = true;
+            }
+            if (dayCounts[i] > highestValue) {
+                highestValue = dayCounts[i];
+                day = i;
+                tied = false;
+            }
+        }
+        return day;
+    }
+
+    // 7.16
+
+    /**
+     * Returns the quietest day.
+     * If there are multiple days tied for quietest, it only gives the first.
+     * With no data, null is returned.
+     *
+     * @return quietest hour
+     */
+    public int quietestDay() {
+        int lowestValue = Integer.MAX_VALUE;
+        int day = -1;
+        boolean tied = false;
+        for (int i = 0; i < dayCounts.length; i++) {
+            if (dayCounts[i] == lowestValue) {
+                tied = true;
+            }
+            if (dayCounts[i] < lowestValue) {
+                lowestValue = dayCounts[i];
+                day = i;
+                tied = false;
+            }
+        }
+        return day;
+    }
+
+    /**
+     * Returns the busiest month.
+     * If there are multiple months tied for busiest, it only returns the first.
+     * With no data, null is returned.
+     *
+     * @return busiest month
+     */
+    public int busiestMonth() {
+        int highestValue = 0;
+        int month = -1;
+        boolean tied = false;
+        for (int i = 0; i < monthCounts.length; i++) {
+            if (monthCounts[i] == highestValue) {
+                tied = true;
+            }
+            if (monthCounts[i] > highestValue) {
+                highestValue = monthCounts[i];
+                month = i;
+                tied = false;
+            }
+        }
+        return month;
+    }
+
+    // 7.16
+
+    /**
+     * Returns the quietest month.
+     * If there are multiple months tied for quietest, it only gives the first.
+     * With no data, null is returned.
+     *
+     * @return quietest month
+     */
+    public int quietestMonth() {
+        int lowestValue = Integer.MAX_VALUE;
+        int month = -1;
+        boolean tied = false;
+        for (int i = 0; i < monthCounts.length; i++) {
+            if (monthCounts[i] == lowestValue) {
+                tied = true;
+            }
+            if (monthCounts[i] < lowestValue) {
+                lowestValue = monthCounts[i];
+                month = i;
+                tied = false;
+            }
+        }
+        return month;
+    }
+
 
     // 7.18
 
     /**
-     * Returns the busiest consecutive two hours in a year.
-     * If there are multiple hours tied for busiest,
-     * it says there's a tie and only gives the first pair.
+     * Returns the busiest consecutive two hours.
+     * If there are multiple hours tied for busiest, it only gives the first pair.
+     * With no data, null is returned.
      *
      * @return busiest hour pair
-     * in a
-     * year
      */
     public int[] busiestTwoHour() {
         int highestValue = 0;
@@ -167,15 +290,18 @@ public class LogAnalyzer {
             }
         }
         if (highestValue == 0) {
-            System.out.println("No data");
             return null;
         }
-        if (!tied) {
-            System.out.println("Hours " + hour + " and " + (hour + 1) + " are the busiest hours with " + highestValue + " combined uses.");
-        } else {
-            System.out.println("Hours " + hour + " and " + (hour + 1) + " are tied for the busiest hours with " + highestValue + " combined uses.");
-        }
         return new int[]{hour, hour + 1};
+    }
+
+    /**
+     * Returns the average amount of data access per month.
+     *
+     * @return the average amount of data access per month
+     */
+    public double averageAccessesPerMonth() {
+        return (double) numberOfAccesses() / 12;
     }
 
 
--- LogfileCreator.java
+++ LogfileCreator.java
@@ -3,61 +3,59 @@ import java.util.*;
 
 /**
  * A class for creating log files of random data.
- * 
+ *
  * @author David J. Barnes and Michael Kölling
- * @version    2016.02.29
+ * @version 2016.02.29
  */
-public class LogfileCreator
-{
+public class LogfileCreator {
     private Random rand;
 
     /**
      * Create log files.
      */
-    public LogfileCreator()
-    {
+    public LogfileCreator() {
         rand = new Random();
     }
-    
+
     /**
      * Create a file of random log entries.
-     * @param filename The file to write.
+     *
+     * @param filename   The file to write.
      * @param numEntries How many entries.
      * @return true if successful, false otherwise.
      */
-    public boolean createFile(String filename, int numEntries)
-    {
+    public boolean createFile(String filename, int numEntries) {
         boolean success = false;
-        
-        if(numEntries > 0) {
+
+        if (numEntries > 0) {
             try (FileWriter writer = new FileWriter(filename)) {
                 LogEntry[] entries = new LogEntry[numEntries];
-                for(int i = 0; i < numEntries; i++) {
+                for (int i = 0; i < numEntries; i++) {
                     entries[i] = createEntry();
                 }
                 Arrays.sort(entries);
-                for(int i = 0; i < numEntries; i++) {
+                for (int i = 0; i < numEntries; i++) {
                     writer.write(entries[i].toString());
                     writer.write('\n');
                 }
-                
+
                 success = true;
-            }
-            catch(IOException e) {
+            } catch (IOException e) {
                 System.err.println("There was a problem writing to " + filename);
             }
-                
+
         }
         return success;
     }
-    
+
     /**
      * Create a single (random) entry for a log file.
+     *
      * @return A log entry containing random data.
      */
-    public LogEntry createEntry()
-    {
-        int year = 2016;
+    public LogEntry createEntry() {
+        // 2015 - 2019
+        int year = 2015 + rand.nextInt(5);
         int month = 1 + rand.nextInt(12);
         // Avoid the complexities of days-per-month.
         int day = 1 + rand.nextInt(28);
